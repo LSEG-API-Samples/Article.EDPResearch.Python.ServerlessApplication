778be513ee24e19f7344b29548b3b6bf005a7620
Veerapath Rungruengrayubkul <veerapath.rungruengrayubkul@thomsonreuters.com>
2019-10-11 16:43:08 +0700

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,16 @@
+$output = [string] (aws iam create-role --role-name lambda-sqs-ssm --assume-role-policy-document file://role.json)
+
+$role_info = ConvertFrom-Json $output
+$arn_info = $role_info.Role.Arn
+
+"Created new Role's ARN:" + $arn_info
+
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --role-name lambda-sqs-ssm
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonSSMFullAccess --role-name lambda-sqs-ssm
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AWSLambdaExecute --role-name lambda-sqs-ssm
+
+aws lambda create-function --function-name getEDPToken --runtime python3.7 --role $arn_info --handler lambda_function.lambda_handler --timeout 5 --zip-file fileb://C:\articles\lambda\getEDPToken.zip --region us-east-1
+aws lambda create-function --function-name subscribeResearch --runtime python3.7 --role $arn_info --handler lambda_function.lambda_handler --timeout 5 --zip-file fileb://C:\articles\lambda\subscribeResearch.zip --region us-east-1
+aws lambda create-function --function-name getCloudCredential --runtime python3.7 --role $arn_info --handler lambda_function.lambda_handler --timeout 3 --zip-file fileb://C:\articles\lambda\getCloudCredential.zip --region us-east-1
+aws lambda create-function --function-name getAlertMessage --runtime python3.7 --role $arn_info --handler lambda_function.lambda_handler --timeout 10 --zip-file fileb://C:\articles\lambda\getAlertMessage.zip --region us-east-1
+aws lambda create-function --function-name downloadDocuments --runtime python3.7 --role $arn_info --handler lambda_function.lambda_handler --timeout 10 --zip-file fileb://C:\articles\lambda\downloadDocuments.zip --region us-east-1
